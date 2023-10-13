1ae29004f01328d4978b98bb089741f89907ca8b
RajaVardhan-coder <raja.kagitha@gmail.com>
2023-10-13 14:21:11 +0530

    AES return only variable

--- crypto/aes.js
+++ crypto/aes.js
@@ -1,14 +1,12 @@
 const crypto = require('crypto');
 
-// Your secret key for encryption
-const secretKey = 'YourSecretKeyHere';
 
 // Encryption function
 function aesEncrypt(text, secretKey) {
   const cipher = crypto.createCipher('aes-256-cbc', secretKey);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
-  return 'Encrypted: ' + encrypted;
+  return encrypted;
 }
 
 // Decryption function
@@ -16,7 +14,7 @@ function aesDecrypt(encryptedText, secretKey) {
   const decipher = crypto.createDecipher('aes-256-cbc', secretKey);
   let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
-  return 'Decrypted: ' + decrypted;
+  return decrypted;
 }
 
 
